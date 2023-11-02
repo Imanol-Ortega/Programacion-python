042c0ec1cec9020f67307f8837a028dc2789565d
Imanol-Ortega <59693036+Imanol-Ortega@users.noreply.github.com>
2023-11-02 16:11:16 -0300

    aaaaaa

--- Vehiculos/main.py
+++ Vehiculos/main.py
@@ -8,7 +8,6 @@ from config import DATABASE_URL
 from fastapi.responses import JSONResponse
 from fastapi import Form
 from fastapi.staticfiles import StaticFiles
-from routers import otras_rutas
 from fastapi.responses import RedirectResponse
 import starlette.status as status
 
@@ -19,14 +18,13 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Configuración de la base de datos
 engine = create_engine(DATABASE_URL)
 # Agrega esta línea para eliminar las tablas existentes
-# Base.metadata.drop_all(bind=engine)
+#Base.metadata.drop_all(bind=engine)
 
 # Luego, usa create_all para crear las tablas según las definiciones de los modelos
 #Base.metadata.create_all(bind=engine)
 
 # Configuración de las plantillas de Jinja2
 templates = Jinja2Templates(directory="templates")
-
 
 def get_db():
     db = Session(bind=engine)
@@ -38,5 +36,30 @@ def get_db():
 def menu(request:Request):
     return templates.TemplateResponse("index.html",{"request":request})
 
+@app.get("/vehiculos/")
+def vehiculos(request:Request,db: Session = Depends(get_db)):
+    brands = db.query(Brand).all()
+    return templates.TemplateResponse("formvehiculo.html",{"request":request,"brands":brands})
+@app.get("/marcas/")
+def marcas(request:Request):
+    return templates.TemplateResponse("formmarca.html",{"request":request})
+@app.get("/modelos/")
+def modelos(request:Request):
+    return templates.TemplateResponse("formmodelo.html",{"request":request})
 
 
+@app.get('/obtener_modelos/{id}')
+async def obtener_modelos(request:Request, id:int,db: Session = Depends(get_db)):
+    models = db.query(Model).filter_by(idMarcaFk = id).all()
+    return JSONResponse(content=[{"idModelo":model.idModelo,"descModelo":model.descModelo} for model in models])
+
+
+
+@app.post('/cargar_vehiculos/')
+async def cargar_vehiculos(request:Request, matricula: str = Form(...),color: str = Form(...),idMarkaFk: int = Form(...), idModeloFk: int=Form(...), image: str = Form(...),db: Session = Depends(get_db)):
+    new_v = Vehicle(matricula = matricula,color=color,foto = image,idMarcaFk=idMarkaFk, idModeloFk=idModeloFk)
+    db.add(new_v)
+    db.commit()
+    return RedirectResponse(
+        '/', 
+        status_code=status.HTTP_302_FOUND)
--- Vehiculos/models.py
+++ Vehiculos/models.py
@@ -8,13 +8,13 @@ Base = declarative_base()
 class Brand(Base):
     __tablename__ = 'brands'
     idMarca = Column(Integer, primary_key=True, index=True)
-    descMarca = Column(String, unique=True, index=True)
+    descMarca = Column(String(50), unique=True, index=True)
     models = relationship("Model", back_populates="brand")
 
 class Model(Base):
     __tablename__ = 'models'
     idModelo = Column(Integer, primary_key=True, index=True)
-    descModelo = Column(String, unique=True, index=True)
+    descModelo = Column(String(50), unique=True, index=True)
     idMarcaFk = Column(Integer, ForeignKey('brands.idMarca'))
     brand = relationship("Brand", back_populates="models")
     vehicles = relationship("Vehicle", back_populates="model")
@@ -22,8 +22,9 @@ class Model(Base):
 class Vehicle(Base):
     __tablename__ = 'vehicles'
     idVehiculo = Column(Integer, primary_key=True, index=True)
-    matricula = Column(String, unique=True, index=True)
-    foto = Column(String)
+    matricula = Column(String(50), unique=True, index=True)
+    color = Column(String(50))
+    foto = Column(String(245))
     idMarcaFk = Column(Integer, ForeignKey('brands.idMarca'))
     idModeloFk = Column(Integer, ForeignKey('models.idModelo'))
     brand = relationship("Brand")
